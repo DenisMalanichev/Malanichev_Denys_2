298c047f7435e934d7e8c4f81b0f8fab48d9a2c3
user <user@WINDOWS-L6FFFT5>
2022-05-10 14:19:01 +0300

    Add project files.

---
+++ lab2.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace lab2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeClass tc = new TimeClass(10, 12, 11);
+
+            string JsonData = JsonConvert.SerializeObject(tc);
+            
+            var tc2 = JsonConvert.DeserializeObject<TimeClass>(JsonData);
+
+            Console.WriteLine(tc2.Hours);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab2.2/TimeClass.cs
@@ -0,0 +1,62 @@
+﻿using System.Threading;
+
+namespace lab2._2
+{
+    internal class TimeClass
+    {
+
+        public int Hours { get; set; }        
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+        public TimeClass(int hours, int minutes, int seconds)
+        {
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = seconds;
+        }
+        public TimeClass()
+        {
+            Hours = 10;
+            Minutes = 12;
+            Seconds = 38;
+        }
+        override public string ToString()
+        {
+            string line = Hours + ":" + Minutes + ":" + Seconds;
+            return line;
+        }
+        public void Clock()
+        {
+            Thread.Sleep(1000);
+            Seconds++;
+            if (Seconds >= 60)
+            {
+                Seconds = 0;
+                Minutes++;
+            }
+            if (Minutes >= 60)
+            {
+                Minutes = 0;
+                Hours++;
+            }
+            if (Hours >= 24)
+            {
+                Hours = 0;
+            }
+            Clock();
+        }
+
+        //Різниця двох відрізків часу в секундах
+        public int Differens(int NHours, int NMins, int NSec)
+        {
+            return Hours * 60 * 60 + Minutes * 60 + Seconds - (NHours * 60 * 60 + NMins * 60 + NSec);
+        }
+        public void AddSec(int sec)
+        {
+            Hours += sec / 60 / 60;
+            Minutes += sec / 60;
+            Seconds += (sec - Hours * 60 * 60 - Minutes * 60);
+        }
+    }
+
+}
